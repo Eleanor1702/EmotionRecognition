74bf05dfbaa64d0e5bb09d563cd6b44e6d3c85b9
noor <eleanor.alrabea@gmail.com>
2019-06-25 12:12:27 +0200

    remove Debug/bin

---
+++ code/EmotionRecognition/bin/Debug/NeuronalNets/train_ckplus.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+import numpy as np
+import random
+import cv2
+import model
+import csv
+
+# Training Parameters
+num_steps = 500
+batch_size = 32
+
+# images and labels
+imgs = []
+labels = []
+img_size = 128
+
+# read csv file
+with open('data/ckplus.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV:
+        pixels = [float(x) for x in row[1].split(' ')]
+        imgs.append(np.array(pixels))
+        labels.append(int(row[0]))
+
+# wrap into np arrays and divide pixels by 255
+imgs = np.array(imgs, dtype=np.float32)
+imgs = np.divide(imgs, 255)
+
+# take a peek at the images
+# for i in imgs:
+#     cv2.imshow('Face', np.reshape(np.array(i), [img_size, img_size]))
+#     cv2.waitKey()
+
+labels = np.array(labels, dtype=np.float32)
+
+# TODO: "leave one out" cross validation?
+
+# use sklearn for dividing dataset randomly (pseudo-random for now..)
+imgs_train, imgs_test, labels_train, labels_test = train_test_split(imgs, labels, test_size=0.2, random_state=42)
+
+# Build the Estimator
+estimator = tf.estimator.Estimator(
+    model_fn=model.model_fn, 
+    model_dir='./model/ckplus',
+    params={
+        'learning_rate': 0.001,
+        'num_classes': 7,
+        'img_size': img_size,
+        'dropout_rate': 0.50
+    })
+
+# Training
+# Define the input function for training
+input_fn = tf.estimator.inputs.numpy_input_fn(
+    x={'images': imgs_train}, y=labels_train,
+    batch_size=batch_size, num_epochs=None, shuffle=True)
+# Train the Model
+estimator.train(input_fn, steps=num_steps)
+
+
+
+# Evaluate the Model
+# Define the input function for evaluating
+input_fn = tf.estimator.inputs.numpy_input_fn(
+    x={'images': imgs_test}, y=labels_test,
+    batch_size=batch_size, shuffle=False)
+# Use the Estimator 'evaluate' method
+estimator.evaluate(input_fn)
+
+# Export the model as a SavedModel for production use
+feature_spec = {'images': tf.placeholder(dtype=tf.float32, shape=[None, img_size * img_size])}
+serving_input_fn = tf.estimator.export.build_raw_serving_input_receiver_fn(feature_spec)
+
+estimator.export_savedmodel(
+    export_dir_base='saved_models/ckplus',
+    serving_input_receiver_fn=serving_input_fn,
+    as_text=True)
---
+++ code/EmotionRecognition/bin/Debug/NeuronalNets/train_ferplus.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+
+import model
+
+# Training Parameters.
+max_steps = 5000
+batch_size = 128
+eval_interval = 240
+
+# Read CSV with pandas.
+ferplus = pd.read_csv('data/ferplus.csv')
+
+# Split and cast pixel intensities to float32.
+ferplus.pixels = ferplus.pixels.str.split()
+ferplus.pixels = ferplus.pixels.map(lambda p: pd.to_numeric(p, downcast='float'))
+
+# Filter out noface class.
+ferplus = ferplus.query('NF == 0')
+# Filter out faces where four or more workers were not sure about the emotion.
+ferplus = ferplus.query('unknown < 4')
+
+# Class labels in the usual order.
+classes = ['anger', 'disgust', 'fear', 'happiness', 'sadness', 'surprise', 'neutral']
+
+# Get argmax of class distribution to use as label.
+labels = ferplus[classes]
+maxlabels = labels.idxmax(axis=1).map(labels.columns.get_loc)
+ferplus.insert(loc=12, column='maxlabel', value=maxlabels)
+
+# Split train, test and validation set.
+train = ferplus.loc[ferplus['Usage'] == 'Training']
+valid = ferplus.loc[ferplus['Usage'] == 'PublicTest']
+test = ferplus.loc[ferplus['Usage'] == 'PrivateTest']
+
+# Prepare input images
+x_test = np.array(test['pixels'].values.tolist())
+x_train = np.array(train['pixels'].values.tolist())
+x_valid = np.array(valid['pixels'].values.tolist())
+
+# Prepate input label distributions
+y_test = test[classes].values
+y_train = train[classes].values
+y_valid = valid[classes].values
+
+
+# Build the Estimator
+estimator = tf.estimator.Estimator(
+    model_fn=model.model_fn, 
+    model_dir='./model/ferplus',
+    params={
+        'learning_rate': 0.001,
+        'num_classes': 7,
+        'img_size': 48,
+        'dropout_rate': 0.3
+})
+
+
+# Define the input function for training.
+train_input_fn = tf.estimator.inputs.numpy_input_fn(
+    x={'images': x_train}, y=y_train,
+    batch_size=batch_size, num_epochs=None, shuffle=True)
+
+# Define the input function for validation during training.
+valid_input_fn = tf.estimator.inputs.numpy_input_fn(
+    x={'images': x_valid}, y=y_valid,
+    batch_size=batch_size, num_epochs=None, shuffle=False)
+
+# Specify training operations.
+train_spec = tf.estimator.TrainSpec(
+    input_fn = train_input_fn,
+    max_steps = max_steps
+)
+
+# Specify evaluation operations.
+eval_spec = tf.estimator.EvalSpec(
+    input_fn = valid_input_fn,
+    throttle_secs=eval_interval,
+    start_delay_secs=eval_interval,
+)
+
+# Train the Model and evaluate periodically
+tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
+
+
+# Define the input function for evaluating with the test set.
+input_fn = tf.estimator.inputs.numpy_input_fn(
+    x={'images': x_test}, y=y_test,
+    batch_size=batch_size, shuffle=False)
+# Evaluate the model accuracy with the test set
+estimator.evaluate(input_fn)
+
+# Export the model as a SavedModel for production use
+feature_spec = {'images': tf.placeholder(dtype=tf.float32, shape=[None, 48 * 48])}
+serving_input_fn = tf.estimator.export.build_raw_serving_input_receiver_fn(feature_spec)
+
+estimator.export_savedmodel(
+    export_dir_base='saved_models/ferplus',
+    serving_input_receiver_fn=serving_input_fn,
+    as_text=True)
